de179cb59ff8f21c730efb6b976df59405f38e89
MarkoMesel <mmarkom0001@gmail.com>
2020-06-02 20:09:06 +0200

    Napravljen docker-compose.yml i Dockerfile-ovi za AgentService, CarService, ScheduleService i UserService. Zapoceta AgentMonolith aplikacija.

---
+++ AgentMonolithApp/AgentMonolith/src/main/java/com/xiws/agentm/AgentMonolithApplication.java
@@ -0,0 +1,13 @@
+package com.xiws.agentm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentMonolithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentMonolithApplication.class, args);
+	}
+
+}
---
+++ AgentMonolithApp/AgentMonolith/src/test/java/com/xiws/agentm/AgentMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiws.agentm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgentMonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
